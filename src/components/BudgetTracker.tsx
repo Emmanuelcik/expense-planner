cc20478ba6cad2661d01a7465c03c2b7924dfa5e
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-07-02 22:48:39 -0600

    made more functional de cirular progressive bar

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,25 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css";
 
 const BudgetTracker = () => {
   const { state, available, totalExpenses } = useBudget();
 
+  const percentage = Number(((totalExpenses / state.budget) * 100).toFixed(2));
+
   return (
     <div className="grid gird-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="Grafic" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage === 100 ? "#DC2626" : "#3b82f6",
+            trailColor: percentage === 100 ? "#DC2626" : "#3b82f6",
+            textSize: 10,
+          })}
+          text={`${percentage}% Spent`}
+        />
       </div>
 
       <div className="flex flex-col justify-center items-center gap-8">
